bb8eb3c34d7744bb8c1d8c4921012c631eefbb19
Nikhil Singh <90076484+TheLazyNick@users.noreply.github.com>
2021-09-04 03:56:29 +0100

    Update 001_EB_Print.py
    Added time calculation module

--- Python/001_EB_Print.py
+++ Python/001_EB_Print.py
@@ -1,1 +1,11 @@
+import datetime
+start = datetime.datetime.now()
+
 print("Hello World")
+    
+    
+"""Calcuation of time"""
+end = datetime.datetime.now()
+total = end-start
+print("Execution time in Seconds:",total.seconds)
+print("Execution time in Miliseconds:",total.microseconds)
