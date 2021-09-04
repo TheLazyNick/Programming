6e696b79ed1fd800bef2c998c21b217ddec05eab
Nikhil Singh <90076484+TheLazyNick@users.noreply.github.com>
2021-09-04 04:29:41 +0100

    Execution time added for python
    Execution time in seconds

--- Python/001_EB_Print_Hello.py
+++ Python/001_EB_Print_Hello.py
@@ -1,11 +1,13 @@
-import datetime
+import datetime, time
 start = datetime.datetime.now()
 
-print("Hello World")
-    
+print("Hello")
+
     
 """Calcuation of time"""
 end = datetime.datetime.now()
 total = end-start
+
 print("Execution time in Seconds:",total.seconds)
-print("Execution time in Miliseconds:",total.microseconds)
+print(total)
+
